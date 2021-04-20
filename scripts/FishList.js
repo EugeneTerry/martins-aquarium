df1439f1070ab4788f11b09270556a7f8cca32c2
EugeneTerry <eugene.terry73@gmail.com>
2021-04-19 23:20:24 -0500

    got rid of html fish

---
+++ scripts/FishList.js
@@ -0,0 +1,26 @@
+import {fish} from 'database'
+
+export const FishList = () => {
+  const fishes = invokeTheRightFunctionHere()
+  const htmlString = '<article class = "fishList">'
+  for (const fish of fishes) {
+    htmlString += `<section class = "fishcard">
+      <div>
+        <img  class="fish__image image--card" src="${fish.image}" />
+      </div>
+      <div class="fish__name">${fish.name}
+      </div>
+      <div class="fish__species">${fish.species}
+      </div>
+      <div class="fish__length">${fish.length}
+      </div>
+      <div class="fish__location">${fish.location}
+      </div>
+      <div class="fish__diet">${fish.eats}
+      </div>
+      </section>
+    `
+    }
+    htmlString += `</article>`
+    return htmlString
+}
